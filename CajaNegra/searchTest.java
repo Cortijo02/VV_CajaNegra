b66122dc5a0e8261f6ec3bc3124b30b9952d09c2
AlvFernandez <124038076+AlvFernandez@users.noreply.github.com>
2023-11-08 10:42:57 +0100

    Update searchTest.java

--- CajaNegra/searchTest.java
+++ CajaNegra/searchTest.java
@@ -10,32 +10,35 @@ import org.junit.jupiter.api.Test;
 public class searchTest {
     BST<Integer> bst;
 
-    @Test
-    void V1() {
+    void assertFindNode(int n) throws DepthException {
+        bst = new BST(n);
+        Node nodo = new Node(n);
+        assertEquals(nodo, bst.search(n));
+    }
+
+    void assertNotFindNode(int n) throws DepthException {
         bst = new BST<>();
-        assertEquals(null, bst.search(-2501));
+        assertNull(bst.search(n));
     }
 
     @Test
-    void V2() throws DepthException {
-        bst = new BST(-2500);
-        Node nodo = new Node(-2500);
-        //Node<Integer> nodo = new Node(-2500);
-        assertEquals(nodo, bst.search(-2500));
-    }
+    void V1() throws DepthException { assertNotFindNode(-2501); }
 
     @Test
-    void V3() {}
+    void V2() throws DepthException { assertFindNode(-2500); }
 
     @Test
-    void V4() {}
+    void V3() throws DepthException { assertFindNode(-2499); }
 
     @Test
-    void V5() {}
+    void V4() throws DepthException { assertFindNode(0); }
 
     @Test
-    void V6() {}
+    void V5() throws DepthException { assertFindNode(2498); }
 
     @Test
-    void V7() {}
+    void V6() throws DepthException { assertFindNode(2499); }
+
+    @Test
+    void V7() throws DepthException { assertNotFindNode(2500); }
 }
