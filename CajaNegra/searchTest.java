36009985ac0fe177078ed2a7a042c1ff3a899816
Dong <itsd0ng.sz@gmail.com>
2023-11-23 11:25:28 +0100

    Excel pruebas search

--- CajaNegra/searchTest.java
+++ CajaNegra/searchTest.java
@@ -1,44 +1,9 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+@org.junit.jupiter.params.ParameterizedTest
+@org.junit.jupiter.params.provider.CsvFileSource(resources= "Pruebas caja negra .csv",
+numLinesToSkip = 1,
+        lineSeparator = "\n",
+        delimiterString = ",")
 
-import com.bst.BST;
-import com.bst.IBinaryTree;
-import com.bst.Node;
-import com.exceptions.DepthException;
-import org.junit.jupiter.api.Test;
+void testSearch(int a, String esperada){
 
-public class searchTest {
-    BST<Integer> bst;
-
-    void assertFindNode(int n) throws DepthException {
-        bst = new BST(n);
-        Node nodo = new Node(n);
-        assertEquals(nodo, bst.search(n));
-    }
-
-    void assertNotFindNode(int n) throws DepthException {
-        bst = new BST<>();
-        assertNull(bst.search(n));
-    }
-
-    @Test
-    void V1() throws DepthException { assertNotFindNode(-2501); }
-
-    @Test
-    void V2() throws DepthException { assertFindNode(-2500); }
-
-    @Test
-    void V3() throws DepthException { assertFindNode(-2499); }
-
-    @Test
-    void V4() throws DepthException { assertFindNode(0); }
-
-    @Test
-    void V5() throws DepthException { assertFindNode(2498); }
-
-    @Test
-    void V6() throws DepthException { assertFindNode(2499); }
-
-    @Test
-    void V7() throws DepthException { assertNotFindNode(2500); }
-}
+        }
