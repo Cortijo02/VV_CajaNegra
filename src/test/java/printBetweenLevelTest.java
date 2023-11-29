8799a156f396e1d9914b529316403b2b165f11e2
Diego <diego.cruz@alumnos.upm.es>
2023-11-29 18:13:39 +0100

    NuevoTest

--- src/test/java/printBetweenLevelTest.java
+++ src/test/java/printBetweenLevelTest.java
@@ -1,2 +1,15 @@
-public class printBetweenLevelTest {
+import com.bst.BST;
+import com.exceptions.DepthException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class printBetweenLevelTest {
+    @org.junit.jupiter.params.ParameterizedTest
+    @org.junit.jupiter.params.provider.CsvFileSource(resources = "Pruebas caja negra (insert).csv",
+            numLinesToSkip = 1,
+            lineSeparator = "\n",
+            delimiterString = ";")
+    void testInsert() {
+
+    }
 }
